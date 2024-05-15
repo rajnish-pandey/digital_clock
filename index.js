60639e7f2a35b148d56abf305d42ab45aa7a6d32
rajnishpandey13words <rajnishpandey13words@gmail.com>
2024-05-15 12:42:29 +0530

    css improvements

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var clockTime = document.getElementById('clock-time');
 var clockDate = document.getElementById('clock-date');
 
 // Array of week days
-var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 // Update time every second
 var timerID = setInterval(updateTime, 1000);
@@ -13,7 +13,7 @@ updateTime();
 function updateTime() {
     var cd = new Date();
     var time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
-    var date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth() + 1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+    var date = zeroPadding(cd.getDate(), 2) + '-' + zeroPadding(cd.getMonth() + 1, 2) + '-' + zeroPadding(cd.getFullYear(), 4) + ' ' + week[cd.getDay()];
     clockTime.textContent = time;
     clockDate.textContent = date;
 }
